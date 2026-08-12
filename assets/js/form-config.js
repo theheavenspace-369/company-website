ad6f8f35345bcc440e2f4fabc9efd320159775d4
theheavenspace-369 <jana369bhavya@gmail.com>
2026-08-12 13:18:47 +0530

    Update form-config.js

--- assets/js/form-config.js
+++ assets/js/form-config.js
@@ -14,8 +14,8 @@
  *
  * Until a key is set here, submissions are only saved locally in each
  * visitor's own browser (assets/js/contact-storage.js) and will NOT reach
- * you - so nothing will be lost, but you won't be notified.
+ * you - so nothing will be lost, but you won't be notified. 
  */
 window.HEAVEN_SPACE_FORM_CONFIG = {
-  web3formsAccessKey: "a2b8d64d-d933-42d2-a1cb-0ec534fbaac2"
+  web3formsAccessKey: "f0ddd4d2-5d6f-4158-9793-d6b817aa75c3"  
 };
